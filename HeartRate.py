c2cd293083b12ecac9bebdd978625ec4c0879418
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-01-05 21:20:08 -0500

    remove initial commit
    remove commit

---
+++ HeartRate.py
@@ -0,0 +1,13 @@
+"""
+V0.1
+September 12/29/2017
+
+Lukas Adamowicz
+
+Python 3.6.2 on Windows 10 with 64-bit Anaconda
+"""
+import numpy as np
+import matplotlib.pyplot as pl
+from scipy import signal, interpolate
+
+class ECGAnalysis
