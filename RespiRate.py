b4758791ff7a3b450e0b3de1d047ada0ca11d898
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-06-14 15:08:32 -0400

    Updated ECGAnalysis_v2 to standard conventions
    Started RespiRate

--- RespiRate.py
+++ RespiRate.py
@@ -6,3 +6,15 @@ Lukas Adamowicz
 
 Python 3.6.5 on Windows 10 with 64-bit Anaconda
 """
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+class RespiRate(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        pass
