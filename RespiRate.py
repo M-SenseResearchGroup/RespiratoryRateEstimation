25330b9d0161ac1e5accdc8a43b4dbab782e32ff
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2018-06-18 12:05:00 -0400

    RespiRate.py -Added save and open functions for serializing and opening the serialized data

--- RespiRate.py
+++ RespiRate.py
@@ -8,10 +8,12 @@ Python 3.6.5 on Windows 10 with 64-bit Anaconda
 """
 
 import sys
+from os import getcwd()
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QAction, qApp, QMessageBox, QMenu
 from PyQt5.QtGui import QIcon
 from ECGAnalysis_v2 import ECGAnalysis, EcgData
 from numpy import loadtxt, array, argmin
+from pickle import load as Pload, dump as Pdump
 
 
 class RespiRate(QMainWindow):
@@ -21,45 +23,95 @@ class RespiRate(QMainWindow):
         self.sbar = self.statusBar()  # new shorthand for status bar
         self.annots = None  # allocate for annotations to allow for checking later on
 
+        self.loc = getcwd()  # get the location of the file.
+
         self.initUI()
 
     def initUI(self):
         self.setGeometry(100, 100, 650, 450)
         self.setWindowTitle('RespiRate')
 
+        # save imported raw data
+        self.saveData = QAction('Save', self)
+        self.saveData.setDisabled(True)  # disable initially because we have no imported data to save
+        self.saveData.setShortcut('Ctrl+S')
+        self.saveData.setStatusTip('Save imported raw data')
+        self.saveData.triggered.connect(self.save_data)
+
+        # Exit menu action
         exitAct = QAction(QIcon('exit.png'), '&Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
 
-        openAnnot = QAction('Import Annotations', self)
-        openAnnot.setStatusTip('Import Annotation File')
-        openAnnot.triggered.connect(self.open_annotation_mc10)
+        # import annotations menu action
+        importAnnot = QAction('Import Annotations', self)
+        importAnnot.setStatusTip('Import Annotation File')
+        importAnnot.triggered.connect(self.open_annotation_mc10)
 
-        openRaw = QAction("Import Raw Data", self)
-        openRaw.setStatusTip('Import Raw ECG Data')
-        openRaw.triggered.connect(self.open_lead_mc10)
+        # import raw data
+        importRaw = QAction("Import Raw Data", self)
+        importRaw.setStatusTip('Import Raw ECG Data')
+        importRaw.triggered.connect(self.open_lead_mc10)
 
+        # import annotation/raw data submenu
         importMenu = QMenu('Import Data', self)
-        importMenu.addAction(openAnnot)
-        importMenu.addAction(openRaw)
+        importMenu.addAction(importAnnot)
+        importMenu.addAction(importRaw)
 
-        self.statusBar()
+        # open previously saved (serialized data) action
+        openData = QAction('Open', self)
+        openData.setShortcut('Ctrl+O')
+        openData.setStatusTip('Open saved data')
+        openData.triggered.connect(self.open_serialized_data)
 
         menubar = self.menuBar()
         filemenu = menubar.addMenu('&File')
 
         filemenu.addMenu(importMenu)
+        filemenu.addAction(openData)
+        filemenu.addAction(self.saveData)
         filemenu.addAction(exitAct)
 
         self.show()
+
+    def save_data(self):
+        """
+        Serialize the data using Pickle for easy importing later
+        """
+
+        saveFile, _ = QFileDialog.getSaveFileName(self, "Save Raw Data", self.loc, "ECG (*.ecg);;All Files (*.*)",
+                                                  options=QFileDialog.Options())
+
+        if saveFile:
+            self.sbar.showMessage('Saving data...')
+            fid = open(saveFile, 'wb')
+            Pdump(self.data, fid)
+            fid.close()
+            self.sbar.clearMessage()
+
+    def open_serialized_data(self):
+        """
+        Open previously serialized data
+        """
+        openFile, _ = QFileDialog.getOpenFileName(self, "Open Data File", self.loc, "ECG (*.ecg);;All Files (*.*)",
+                                                  options=QFileDialog.Options())
+
+        if openFile:
+            self.sbar.showMessage('Opening data file...')
+            fid = open(openFile,'rb')
+            self.data = Pload(fid)
+            fid.close()
+            self.sbar.clearMessage()
+
+            self.saveData.setDisabled(False)
 
     def open_annotation_mc10(self):
         """
         Opens MC10 activity annotation file and then displays how many activities were found
         """
 
-        # for easy testing
+        # TODO remove home_dir, which is used only for testing
         home_dir = "C:\\Users\\Lukas Adamowicz\\Dropbox\\Masters\\Project - Bike Study\\RespiratoryRate_HeartRate\\" +\
                    "RespiratoryRateEstimation\\Validation_Data\\RespRate_PPG_Phone\\DN"
 
@@ -68,6 +120,7 @@ class RespiRate(QMainWindow):
                                                         options=QFileDialog.Options())
 
         if annotationFile:
+            self.sbar.showMessage('Importing annotations...')  # show message while importing annotations
             # import annotation data
             events, starts, stops = loadtxt(annotationFile, delimiter=',', usecols=(2, 4, 5), skiprows=1, unpack=True,
                                             dtype=str)
@@ -77,12 +130,14 @@ class RespiRate(QMainWindow):
             # display a message showing how many events were found
             QMessageBox.question(self, "Annotation Import", f"Found {len(self.annots.keys())} events.", QMessageBox.Ok)
 
+        self.sbar.clearMessage()  # clear the message when done importing
+
     def open_lead_mc10(self):
         """
         Opens MC10 ECG lead data
         """
 
-        # for easy testing
+        # TODO remove home_dir, which is used only for testing
         home_dir = "C:\\Users\\Lukas Adamowicz\\Dropbox\\Masters\\Project - Bike Study\\RespiratoryRate_HeartRate\\" + \
                    "RespiratoryRateEstimation\\Validation_Data\\RespRate_PPG_Phone\\DN\\ecg_lead_ii\\d5la7xul\\" +\
                    "2018-01-24T03-18-36-159Z"
@@ -92,6 +147,7 @@ class RespiRate(QMainWindow):
                                                   options=QFileDialog.Options())
 
         if leadFile:
+            self.sbar.showMessage('Importing raw data...')  # show status bar message while importing data
             self.data = EcgData()  # object for storing ECG data
             if isinstance(self.annots, type(None)):  # check if self.annots was defined by importing a file or not
                 self.data.t['all'], self.data.v['all'] = loadtxt(leadFile, delimiter=',', skiprows=1, unpack=True,
@@ -109,6 +165,8 @@ class RespiRate(QMainWindow):
 
                 QMessageBox.question(self, "Raw Data Import", f"Imported raw data and segmented into " +\
                                      f"{len(self.data.t.keys())} events.", QMessageBox.Ok)
+            self.saveData.setDisabled(False)
+        self.sbar.clearMessage()  # clear the status bar message when done importing
 
 
 # below code allows exceptions to be reported for debugging the program
